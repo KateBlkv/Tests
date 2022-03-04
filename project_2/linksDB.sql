064d53aeceed9aafccb01b7bdf8c2709958f0689
Katya <trapam@yandex.ru>
2022-03-05 01:37:37 +0300

    +mysql

--- project_2/linksDB.sql
+++ project_2/linksDB.sql
@@ -4,7 +4,7 @@ use all_links;
 create table if not exists tb_links
 (
     id    int auto_increment primary key,
-    long_link  varchar(255) not null,
+    long_link  text not null,
     short_link varchar(50)  not null,
     ip varchar(50),
     data_time int,
